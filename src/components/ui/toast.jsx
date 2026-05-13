7fcc25fdf39e14b889f8a196d43b0f10865afc07
Jules Avila <julioavilasaavedra@gmail.com>
2026-05-13 00:18:48 +0000

    File changes

--- src/components/ui/toast.jsx
+++ src/components/ui/toast.jsx
@@ -7,6 +7,7 @@ const ToastProvider = React.forwardRef(({ ...props }, ref) => (
   <div
     ref={ref}
     className="fixed top-0 z-[100] flex max-h-[10px] w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]"
+    style={{ top: '-20px' }}
     {...props}
   />
 ));
